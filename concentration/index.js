3b0c31c1950ef8d7342fdd85c3719939477f86d9
naoido <54303857+naoido@users.noreply.github.com>
2022-08-29 20:28:25 +0900

    Update index.js

--- concentration/index.js
+++ concentration/index.js
@@ -1,6 +1,6 @@
 const field = document.getElementById("field");
 
-const cards = [1,2,3,4,5,6,7,8,9,10,11,12];
+const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 const cardsType = {
     "full": ["Spade", "Clover", "Heart", "Diamond"],
     "half": ["Spade", "Clover"]
